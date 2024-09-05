d23613b87cb477be675c7dffdb32e3310d48a683
Nevos <leonossowski08@gmail.com>
2024-09-05 17:47:44 +0200

    add shadcn

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       '@shared': path.resolve(__dirname, '../game/shared'),
     },
   },
